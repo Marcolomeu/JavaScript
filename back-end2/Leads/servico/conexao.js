19ec1d90b13e71d6bf5ee9ad566bb547bf4fade6
Marcolomeu <marcoslopescassimiro@gmail.com>
2025-02-26 18:38:08 -0400

    a

--- back-end2/Leads/servico/conexao.js
+++ back-end2/Leads/servico/conexao.js
@@ -3,8 +3,8 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
     host     : 'localhost',
     user     : 'root',
-    password : 'Suporte99',
-    database : 'Leads'
+    password : '0101',
+    database : 'leads'
 });
 
 export default pool;
